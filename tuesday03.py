6df4b9eb441bc7cca5d0dde1bc597f37bcf1f412
Gerardo Garza <gerardo120386@gmail.com>
2022-01-04 00:40:10 -0600

    Version 1.0

--- tuesday03.py
+++ tuesday03.py
@@ -77,8 +77,8 @@ else:
 
 
 
-row2_spacer1, row2_spacer2, row2_spacer3 = st.columns(
-    (.1, .01,.1)
+row2_spacer1, row2_spacer2,row2_spacer2_2, row2_spacer3 = st.columns(
+    (.1, .1,.1,.1)
     )
 
 
