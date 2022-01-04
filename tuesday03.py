c5c4af7c914c2621210fd69f6a81bb47f9835f5f
Gerardo Garza <gerardo120386@gmail.com>
2022-01-04 00:25:33 -0600

    Version 1.0

--- tuesday03.py
+++ tuesday03.py
@@ -3,21 +3,21 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-#CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# #CSS
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+# def remote_css(url):
+#     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
-def icon(icon_name):
-    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+# def icon(icon_name):
+#     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
-local_css("style.css")
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+# local_css("style.css")
+# remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-button_clicked = st.button("Re-load")
+# button_clicked = st.button("Re-load")
 
 
 df = pd.read_csv('my_new_portfolio.csv')
